264ad50af69ce29e8001d3126a396a5d2a41bcf2
erenay44 <erenayceyran@gmail.com>
2026-07-24 16:07:57 +0300

    DbContext

---
+++ DataLayer/Contexts/AgricultureContext.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Contexts
+{
+    public class AgricultureContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=localhost\\SQLEXPRESS;database=AgricultureDb;integrated security = true");
+
+        }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Announcement> Announcements { get; set; }
+        public DbSet<Service> Services { get; set; }
+    }
+}
--- EntityLayer/Concrete/Announcement.cs
+++ EntityLayer/Concrete/Announcement.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace EntityLayer.Concrete
 {
-    public class News
+    public class Announcement
     {
-        public int NewsId { get; set; }
+        public int AnnouncementId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTime Date { get; set; }
