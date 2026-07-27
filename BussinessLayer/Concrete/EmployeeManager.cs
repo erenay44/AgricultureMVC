777aed9b61ca48980193bd4d4899f1db25d07d49
erenay44 <erenayceyran@gmail.com>
2026-07-27 16:41:55 +0300

    implement EmployeeManager with dependency injection

---
+++ BussinessLayer/Concrete/EmployeeManager.cs
@@ -0,0 +1,46 @@
+﻿using BussinessLayer.Abstract;
+using DataLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.Concrete
+{
+    public class EmployeeManager:IEmployeeService
+    {
+        private readonly IEmployeeDal _employeeDal;
+
+        public EmployeeManager(IEmployeeDal employeeDal)
+        {
+            _employeeDal = employeeDal;
+        }
+
+        public void Delete(Employee t)
+        {
+           _employeeDal.Delete(t);
+        }
+
+        public Employee GetById(int id)
+        {
+            return _employeeDal.GetById(id);
+        }
+
+        public List<Employee> GetListAll()
+        {
+            return _employeeDal.GetListAll();
+        }
+
+        public void Insert(Employee t)
+        {
+            _employeeDal.Insert(t);
+        }
+
+        public void Update(Employee t)
+        {
+            _employeeDal.Update(t);
+        }
+    }
+}
