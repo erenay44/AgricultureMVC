e67a74ebc591e7a51fe0ba878dacdeac6aa61a0d
erenay44 <erenayceyran@gmail.com>
2026-07-28 12:22:20 +0300

    Razor View denemeleri

---
+++ AgricultureUI/Controllers/DefaultController.cs
@@ -0,0 +1,16 @@
+﻿using BussinessLayer.Concrete;
+using DataLayer.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgricultureUI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        ServiceManager serviceManager = new ServiceManager(new EfServiceDal());
+        public IActionResult Index()
+        {
+            var values = serviceManager.GetListAll();
+            return View(values);
+        }
+    }
+}
