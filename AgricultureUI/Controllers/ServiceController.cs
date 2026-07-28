f1c57d43ca0c2ea61d403fcb9eec977f357bcaa7
erenay44 <erenayceyran@gmail.com>
2026-07-28 15:17:00 +0300

    integrate ServiceAddViewModel and validation to AddService view

---
+++ AgricultureUI/Controllers/ServiceController.cs
@@ -0,0 +1,52 @@
+﻿using AgricultureUI.Models;
+using BussinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgricultureUI.Controllers
+{
+    public class ServiceController : Controller
+    {
+        private readonly IServiceService _serviceService;
+
+        public ServiceController(IServiceService serviceService)
+        {
+            _serviceService = serviceService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _serviceService.GetListAll();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddService()
+        {
+            return View(new ServiceAddViewModel());
+        }
+        [HttpPost]
+        public IActionResult AddService(ServiceAddViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _serviceService.Insert(new Service()
+                {
+                    Title = model.Title,
+                    Description = model.Description,
+                    Image = model.Image
+                });
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+        [HttpDelete]
+        public IActionResult DeleteService(int id)
+        {
+            var values = _serviceService.GetById(id);
+            _serviceService.Delete(values);
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
