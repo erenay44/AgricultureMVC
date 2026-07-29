65ad16c735e75daf14260ccb4a01a98db93fe5e2
erenay44 <erenayceyran@gmail.com>
2026-07-29 15:55:45 +0300

    Image manager added

---
+++ BussinessLayer/Concrete/ImageManager.cs
@@ -0,0 +1,46 @@
+﻿using BussinessLayer.Abstract;
+using DataLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.Concrete
+{
+    public class ImageManager:IIMageService
+    {
+        private readonly IImageDal _imageDal;
+
+        public ImageManager(IImageDal imageDal)
+        {
+            _imageDal = imageDal;
+        }
+
+        public void Delete(Image t)
+        {
+            _imageDal.Delete(t);
+        }
+
+        public Image GetById(int id)
+        {
+            return _imageDal.GetById(id);
+        }
+
+        public List<Image> GetListAll()
+        {
+            return _imageDal.GetListAll();
+        }
+
+        public void Insert(Image t)
+        {
+            _imageDal.Insert(t);
+        }
+
+        public void Update(Image t)
+        {
+            _imageDal.Update(t);
+        }
+    }
+}
