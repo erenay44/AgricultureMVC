f28931bb4bafa647f78214debd83f02728921855
erenay44 <erenayceyran@gmail.com>
2026-07-29 15:00:57 +0300

    Announcement Manager added for controller

---
+++ BussinessLayer/Concrete/AnnouncementManager.cs
@@ -0,0 +1,47 @@
+﻿using BussinessLayer.Abstract;
+using DataLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.Concrete
+{
+    public class AnnouncementManager : IAnnouncementService
+    {
+        private readonly IAnnouncementDal _announcementDal;
+
+        public AnnouncementManager(IAnnouncementDal announcementDal)
+        {
+            _announcementDal = announcementDal;
+        }
+
+        public void Delete(Announcement t)
+        {
+            _announcementDal.Delete(t); 
+        }
+
+        public Announcement GetById(int id)
+        {
+           return _announcementDal.GetById(id);
+        
+        }
+
+        public List<Announcement> GetListAll()
+        {
+           return _announcementDal.GetListAll();
+        }
+
+        public void Insert(Announcement t)
+        {
+            _announcementDal.Insert(t);
+        }
+
+        public void Update(Announcement t)
+        {
+            _announcementDal.Update(t);
+        }
+    }
+}
