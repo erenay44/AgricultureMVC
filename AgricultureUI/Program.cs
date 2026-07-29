841ac75ccd61848d7567cd9ea26c01c5e4558242
erenay44 <erenayceyran@gmail.com>
2026-07-29 15:01:42 +0300

    program.cs changed for controller

--- AgricultureUI/Program.cs
+++ AgricultureUI/Program.cs
@@ -12,6 +12,9 @@ builder.Services.AddScoped<IServiceService, ServiceManager>();
 builder.Services.AddScoped<IServiceDal, EfServiceDal>();
 builder.Services.AddScoped<IEmployeeService, EmployeeManager>();
 builder.Services.AddScoped<IEmployeeDal, EfEmployeeDal>();
+builder.Services.AddScoped<IAnnouncementService, AnnouncementManager>();
+builder.Services.AddScoped<IAnnouncementDal, EfAnnouncementDal>();
+
 builder.Services.AddDbContext<AgricultureContext>();
 var app = builder.Build();
 
