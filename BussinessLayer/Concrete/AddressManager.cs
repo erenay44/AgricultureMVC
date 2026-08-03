4fe0dbbe5d0f7d4c81211d133b8a84a464eda2b7
erenay44 <erenayceyran@gmail.com>
2026-08-03 14:24:03 +0300

    AddressManager added

---
+++ BussinessLayer/Concrete/AddressManager.cs
@@ -0,0 +1,48 @@
+﻿using BussinessLayer.Abstract;
+using DataLayer.Abstract;
+using DataLayer.Concrete.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.Concrete
+{
+    public class AddressManager:IAddressService
+    {
+        private readonly IAddressDal _addressDal;
+
+        public AddressManager(IAddressDal addressDal)
+        {
+            _addressDal = addressDal;
+        }
+
+        public void Delete(Address t)
+        {
+            _addressDal.Delete(t);
+        }
+
+        public Address GetById(int id)
+        {
+            return _addressDal.GetById(id);
+        }
+
+        public List<Address> GetListAll()
+        {
+            return _addressDal.GetListAll();
+        }
+
+        public void Insert(Address t)
+        {
+            _addressDal.Insert(t);
+        }
+
+        public void Update(Address t)
+        {
+            _addressDal.Update(t);
+        }
+    }
+}
