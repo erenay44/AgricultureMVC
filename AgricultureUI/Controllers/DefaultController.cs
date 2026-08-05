890f9b8d7d423f25382b3ae09723bbe7ce7bbb61
erenay44 <erenayceyran@gmail.com>
2026-08-05 11:02:09 +0300

    template added

--- AgricultureUI/Controllers/DefaultController.cs
+++ AgricultureUI/Controllers/DefaultController.cs
@@ -6,11 +6,9 @@ namespace AgricultureUI.Controllers
 {
     public class DefaultController : Controller
     {
-        ServiceManager serviceManager = new ServiceManager(new EfServiceDal());
         public IActionResult Index()
         {
-            var values = serviceManager.GetListAll();
-            return View(values);
+            return View();
         }
     }
 }
