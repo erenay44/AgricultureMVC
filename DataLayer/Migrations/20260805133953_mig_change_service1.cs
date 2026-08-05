e1be8f7b656160f556f692a7e018c397920ef613
erenay44 <erenayceyran@gmail.com>
2026-08-05 16:52:55 +0300

    Service Partial done

---
+++ DataLayer/Migrations/20260805133953_mig_change_service1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_change_service1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Class1",
+                table: "Services",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Class2",
+                table: "Services",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Class1",
+                table: "Services");
+
+            migrationBuilder.DropColumn(
+                name: "Class2",
+                table: "Services");
+        }
+    }
+}
