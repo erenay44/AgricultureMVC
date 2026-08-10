38b2c4f294dae655de2a88bc5b30e6af74ea4cae
erenay44 <erenayceyran@gmail.com>
2026-08-10 16:19:46 +0300

    Address Partial

---
+++ AgricultureUI/ViewComponents/_AddressPartial.cs
@@ -0,0 +1,21 @@
+﻿using BussinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace AgricultureUI.ViewComponents
+{
+    public class _AddressPartial:ViewComponent
+    {
+        private readonly IAddressService _addressService;
+
+        public _AddressPartial(IAddressService addressService)
+        {
+            _addressService = addressService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var values = _addressService.GetListAll();
+            return View(values);
+        }
+    }
+}
