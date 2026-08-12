f913e2a5fb0410f251db6d0813ef95714ec5f994
erenay44 <erenayceyran@gmail.com>
2026-08-12 16:37:00 +0300

    Contact model

--- AgricultureUI/Controllers/ReportController.cs
+++ AgricultureUI/Controllers/ReportController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AgricultureUI.Models;
+using ClosedXML.Excel;
+using DataLayer.Contexts;
+using Microsoft.AspNetCore.Mvc;
 using OfficeOpenXml;
 
 namespace AgricultureUI.Controllers
@@ -11,29 +14,81 @@ namespace AgricultureUI.Controllers
         }
         public IActionResult StaticReport()
         {
-            ExcelPackage excelPackage = new ExcelPackage();
-            var workBook = excelPackage.Workbook.Worksheets.Add("Dosya1");
+            using (var workBook = new XLWorkbook())
+            {
+                var workSheet = workBook.Worksheets.Add("Dosya1");
 
-            workBook.Cells[1, 1].Value = "Ürün Adı";
-            workBook.Cells[1, 2].Value = "Ürün Kategorisi";
-            workBook.Cells[1, 3].Value = "Ürün Stok";
+                workSheet.Cell(1, 1).Value = "Ürün Adı";
+                workSheet.Cell(1, 2).Value = "Ürün Kategorisi";
+                workSheet.Cell(1, 3).Value = "Ürün Stok";
 
-            workBook.Cells[2, 1].Value = "Mercimek";
-            workBook.Cells[2, 2].Value = "Bakliyat";
-            workBook.Cells[2, 3].Value = "785 Kg";
+                workSheet.Cell(2, 1).Value = "Mercimek";
+                workSheet.Cell(2, 2).Value = "Bakliyat";
+                workSheet.Cell(2, 3).Value = "785 Kg";
 
-            workBook.Cells[3, 1].Value = "Buğday";
-            workBook.Cells[3, 2].Value = "Bakliyat";
-            workBook.Cells[3, 3].Value = "1.986 Kg";
+                workSheet.Cell(3, 1).Value = "Buğday";
+                workSheet.Cell(3, 2).Value = "Bakliyat";
+                workSheet.Cell(3, 3).Value = "1.986 Kg";
 
-            workBook.Cells[4, 1].Value = "Havuç";
-            workBook.Cells[4, 2].Value = "Sebze";
-            workBook.Cells[4, 3].Value = "167 Kg";
+                workSheet.Cell(4, 1).Value = "Havuç";
+                workSheet.Cell(4, 2).Value = "Sebze";
+                workSheet.Cell(4, 3).Value = "167 Kg";
 
-            var bytes = excelPackage.GetAsByteArray();
-            return File(bytes, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "BakliyatRaporu.xlsx");
+                using (var stream = new MemoryStream())
+                {
+                    workBook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "BakliyatRaporu.xlsx");
+                }
+            }
         }
 
+        public List<ContactModel> ContactList()
+        {
+            List<ContactModel> contactModels = new List<ContactModel>();
+            using (var context = new AgricultureContext())
+            {
+                contactModels = context.Contacts.Select(x => new ContactModel
+                {
+                    ContactId = x.ContactID,
+                    ContactName = x.Name,
+                    ContactDate = x.Date,
+                    ContactMail = x.Mail,
+                    ContactMessage = x.Message
+                }).ToList();
+            }
+            return contactModels;
+        }
+        public IActionResult ContactReport()
+        {
+            using (var workBook = new XLWorkbook())
+            {
+                var workSheet = workBook.Worksheets.Add("Mesaj Listesi");
+                workSheet.Cell(1, 1).Value = "Mesaj ID";
+                workSheet.Cell(1, 2).Value = "Mesaj Gönderen";
+                workSheet.Cell(1, 3).Value = "Mail Adresi";
+                workSheet.Cell(1, 4).Value = "Mesaj İçeriği";
+                workSheet.Cell(1, 5).Value = "Mesaj Tarihi";
+
+                int contactRowCount = 2;
+                foreach (var item in ContactList())
+                {
+                    workSheet.Cell(contactRowCount, 1).Value = item.ContactId;
+                    workSheet.Cell(contactRowCount, 2).Value = item.ContactName;
+                    workSheet.Cell(contactRowCount, 3).Value = item.ContactMail;
+                    workSheet.Cell(contactRowCount, 4).Value = item.ContactMessage;
+                    workSheet.Cell(contactRowCount, 5).Value = item.ContactDate;
+                    contactRowCount++;
+                }
+
+                using (var stream = new MemoryStream())
+                {
+                    workBook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "MesajRapor.xlsx");
+                }
+            }
+        }
 
     }
 
