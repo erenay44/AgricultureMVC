f1fec35ff5efdb965daeb4254acdb46ba4fe5721
erenay44 <erenayceyran@gmail.com>
2026-08-12 16:07:06 +0300

    ChartController

---
+++ AgricultureUI/Controllers/ChartController.cs
@@ -0,0 +1,44 @@
+﻿using AgricultureUI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgricultureUI.Controllers
+{
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ProductChart()
+        {
+            List<ProductClass> productClasses = new List<ProductClass>();
+            productClasses.Add(new ProductClass
+            {
+                productname = "Buğday",
+                productvalue = 850
+            });
+            productClasses.Add(new ProductClass
+            {
+                productname = "Mercimek",
+                productvalue = 480
+            });
+            productClasses.Add(new ProductClass
+            {
+                productname = "Arpa",
+                productvalue = 250
+            });
+            productClasses.Add(new ProductClass
+            {
+                productname = "Pirinç",
+                productvalue = 990
+            });
+            productClasses.Add(new ProductClass
+            {
+                productname = "Domates",
+                productvalue = 960
+            });
+            return Json(new { jsonlist = productClasses });
+
+        }
+    }
+}
---
+++ AgricultureUI/Models/ProductClass.cs
@@ -0,0 +1,8 @@
+﻿namespace AgricultureUI.Models
+{
+    public class ProductClass
+    {
+        public string productname { get; set; }
+        public int productvalue { get; set; }
+    }
+}
