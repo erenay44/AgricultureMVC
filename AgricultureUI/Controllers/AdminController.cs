27e88165cc0b4ce21b8de88e938d0bcd73e13a82
erenay44 <erenayceyran@gmail.com>
2026-08-12 13:05:49 +0300

    Admin Controller

---
+++ AgricultureUI/Controllers/AdminController.cs
@@ -0,0 +1,52 @@
+﻿using BussinessLayer.Abstract;
+using BussinessLayer.ValidationRules;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgricultureUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IAdminService _adminService;
+
+        public AdminController(IAdminService adminService)
+        {
+            _adminService = adminService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _adminService.GetListAll();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddAdmin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddAdmin(Admin admin)
+        {
+                _adminService.Insert(admin);
+                return RedirectToAction("Index");          
+        }
+        public IActionResult DeleteAdmin(int id)
+        {
+            var values = _adminService.GetById(id);
+            _adminService.Delete(values);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult EditAdmin(int id)
+        {
+            var values = _adminService.GetById(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult EditAdmin(Admin admin)
+        {
+            _adminService.Update(admin);
+            return RedirectToAction("Index");
+        }
+    }
+}
