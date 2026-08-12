fc8ef90e50e182060757e9f0303d4d13d2318e79
erenay44 <erenayceyran@gmail.com>
2026-08-12 12:05:49 +0300

    MapPartial added

---
+++ AgricultureUI/ViewComponents/_MapPartial.cs
@@ -0,0 +1,16 @@
+﻿using DataLayer.Contexts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgricultureUI.ViewComponents
+{
+    public class _MapPartial:ViewComponent
+    {
+       public IViewComponentResult Invoke()
+        {
+            AgricultureContext context = new AgricultureContext();
+            var values = context.Addresses.Select(x => x.MapInfo).FirstOrDefault();
+            ViewBag.v = values;
+            return View();
+        }
+    }
+}
