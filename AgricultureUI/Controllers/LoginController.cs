bc96b400c11f2b69cee445409591a7f227b1aa16
erenay44 <erenayceyran@gmail.com>
2026-08-18 00:01:15 +0300

    Login controller changed for identity library and css added

--- AgricultureUI/Controllers/LoginController.cs
+++ AgricultureUI/Controllers/LoginController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AgricultureUI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AgricultureUI.Controllers
@@ -6,9 +9,48 @@ namespace AgricultureUI.Controllers
     [AllowAnonymous]
     public class LoginController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public LoginController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult SignUp() 
+        {
+            return View(); 
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignUp(RegisterViewModel registerViewModel)
+        {
+            IdentityUser identityUser = new IdentityUser()
+            {
+                Id = "1",
+                UserName = registerViewModel.userName,
+                Email = registerViewModel.mail
+            };
+            if (registerViewModel.password == registerViewModel.passwordConfirm)
+            {
+                var result = await _userManager.CreateAsync(identityUser, registerViewModel.password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach(var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View(registerViewModel);
         }
     }
 }
---
+++ AgricultureUI/Models/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgricultureUI.Models
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage ="Lütfen kullanıcı adı giriniz!")]
+        public string userName { get; set; }
+
+        [Required(ErrorMessage = "Lütfen mail giriniz!")]
+        public string mail { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifre giriniz!")]
+        public string password { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifreyi tekrar giriniz giriniz!")]
+        [Compare("password",ErrorMessage ="Şifreler uyumlu değil!")]
+        public string passwordConfirm { get; set; }
+    }
+}
