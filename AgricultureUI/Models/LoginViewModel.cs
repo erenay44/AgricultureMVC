e1f11a6bd9d8f54140fdd6c1ac67e41aff0f3de2
erenay44 <erenayceyran@gmail.com>
2026-08-18 03:06:02 +0300

    Login controller and loginviewmodel added for authentication

---
+++ AgricultureUI/Models/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgricultureUI.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage="Lütfen kullanıcı adını giriniz")]
+        public string username { get; set; }
+        [Required(ErrorMessage = "Lütfen şifre giriniz")]
+        public string password { get; set; }
+    }
+}
