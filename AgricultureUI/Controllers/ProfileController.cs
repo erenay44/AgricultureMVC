1b68695919b1f5250f5a013b6d91363d54a2929a
erenay44 <erenayceyran@gmail.com>
2026-08-18 03:18:08 +0300

    Profile settings page added

---
+++ AgricultureUI/Controllers/ProfileController.cs
@@ -0,0 +1,25 @@
+﻿using AgricultureUI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgricultureUI.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public ProfileController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            UserEditViewModel userEditViewModel = new UserEditViewModel();
+            userEditViewModel.Phone = values.PhoneNumber;
+            userEditViewModel.Mail = values.Email;
+            return View(userEditViewModel);
+        }
+    }
+}
